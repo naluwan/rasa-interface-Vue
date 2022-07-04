2d38f8c069b2d366a08cf7464259504cee44b96e
naluwn <street.baller06@gmail.com>
2022-07-04 12:42:17 +0800

    feat: add Story & RasaConfig views

---
+++ src/views/RasaConfig.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container py-5">
+    <NavTabs />
+    <h1 class="mt-5">
+      Rasa Config
+    </h1>
+  </div>
+</template>
+
+<script>
+import NavTabs from '../components/NavTabs.vue'
+export default {
+  name: 'RasaConfig',
+  components: {
+    NavTabs
+  }
+}
+</script>
---
+++ src/views/Story.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container py-5">
+    <NavTabs />
+    <h1 class="mt-5">
+      顯示故事
+    </h1>
+  </div>
+</template>
+
+<script>
+import NavTabs from '../components/NavTabs.vue'
+export default {
+  name: 'Story',
+  components: {
+    NavTabs
+  }
+}
+</script>
