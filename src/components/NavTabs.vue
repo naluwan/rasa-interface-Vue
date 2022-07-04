cf0dde867ed55ee8c7aecbe0b6e3a44b27634440
naluwn <street.baller06@gmail.com>
2022-07-04 12:43:21 +0800

    feat: add NavTabs component

---
+++ src/components/NavTabs.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="nav nav-tabs mb-4">
+    <li class="nav-item">
+      <router-link
+        to="/story"
+        class="nav-link"
+      >
+        故事流程
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link
+        to="/config"
+        class="nav-link"
+      >
+        Rasa Config
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'NavTabs'
+}
+</script>
