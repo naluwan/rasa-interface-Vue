d316bb2f7eaeebd225778cf855c390d99f638315
naluwn <street.baller06@gmail.com>
2022-07-06 15:00:55 +0800

    feat: add AddUserStep component

---
+++ src/components/AddUserStep.vue
@@ -0,0 +1,73 @@
+<template>
+  <form
+    @submit.stop.prevent="handleSubmit"
+    class="col d-flex justify-content-start"
+  >
+    <div class="userStepContainer col-md-6">
+      <h1 class="h4 mb-3 font-weight-normal">使用者:</h1>
+      <div class="form-label-group mb-2">
+        <label for="user">對話</label>
+        <textarea
+          id="user"
+          name="user"
+          v-model="user"
+          type="text"
+          class="form-control"
+          placeholder="使用者說..."
+          autocomplete="user"
+          required
+          autofocus
+          rows="1"
+        ></textarea>
+      </div>
+      <div class="form-label-group mb-2">
+        <label for="user">意圖</label>
+        <input
+          id="intent"
+          name="intent"
+          v-model="intent"
+          type="text"
+          class="form-control"
+          placeholder="對話意圖"
+          autocomplete="intent"
+          required
+          autofocus
+        />
+      </div>
+      <div class="stepActionBtns mt-3 d-flex justify-content-end">
+        <button class="btn btn-info">送出</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddUserStep",
+  props: {
+    stepIndex: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      user: "",
+      intent: "",
+      entities: [],
+    };
+  },
+  methods: {
+    handleSubmit() {
+      // TODO: 向伺服器API發送POST請求
+      // 伺服器新增資料後...
+      this.$emit("after-create-userStep", {
+        user: this.user,
+        intent: this.intent,
+        entities: this.entities,
+        stepIndex: this.stepIndex,
+      });
+    },
+  },
+};
+</script>
